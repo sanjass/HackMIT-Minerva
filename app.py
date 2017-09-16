e2c33d2de87f3790b44dea66c6e9e20c9a329fdf
sanjas <sanjas@mit.edu>
2017-09-16 15:14:34 -0400

    changed routing and added sign IN file

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def home():
 @app.route("/signup")
 def signup():
     return render_template('signup.html')
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
 if __name__ == "__main__":
     app.run(debug=True, port=2000)
