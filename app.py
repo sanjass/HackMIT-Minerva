f75f170a3c3e28d06103afd90937c6c73d1bad2f
sanjas <sanjas@mit.edu>
2017-09-16 22:36:56 -0400

    changed the main file

--- app.py
+++ app.py
@@ -1,46 +1,75 @@
 from flask import Flask, render_template, json, request
-app = Flask(__name__)
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+mysql = MySQL()
 
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'whatever_your_password_is'
+app.config['MYSQL_DATABASE_DB'] = 'letspset_sanja'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
 @app.route("/")
 def main():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route("/home")
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route("/signup")
 def signup():
 	 
-    return render_template('signup.html')
+	return render_template('signup.html')
 
-@app.route("/signUp",methods=['POST'])
+@app.route("/signUp",methods=['POST','GET'])
 def signUp():
-	# read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
-	# validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good  for sign up!!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+	try:
+		_name = request.form['inputName']
+		_email = request.form['inputEmail']
+		_password = request.form['inputPassword']
 
+		# validate the received values
+		if _name and _email and _password:
+			
+			# All Good, let's call MySQL
+			
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			_hashed_password = generate_password_hash(_password)
+			cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+			data = cursor.fetchall()
+
+			if len(data) is 0:
+				conn.commit()
+				return json.dumps({'message':'User created successfully !'})
+			else:
+				return json.dumps({'error':str(data[0])})
+		else:
+			return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+	except Exception as e:
+		return json.dumps({'error':str(e)})
+	finally:
+		cursor.close() 
+		conn.close()
 
 @app.route("/signin")
 def signin():
-    return render_template('signin.html')
+	return render_template('signin.html')
 
 @app.route("/signIn",methods=['POST'])
 def signIn():
 	# read the posted values from the UI
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
 	# validate the received values
-    if _email and _password:
-        return json.dumps({'html':'<span>All fields good for sign in !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+	if _email and _password:
+		return json.dumps({'html':'<span>All fields good for sign in !!</span>'})
+	else:
+		return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=2000)
+	app.run(debug=True, port=2000)
